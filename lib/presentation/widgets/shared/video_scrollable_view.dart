13031b3efe7eef89c51330f508bba088bd165243
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-05-07 14:06:35 -0400

    FEAT: intl dependency added & animate_do

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableVideo extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,17 +9,33 @@ class VideoScrollableVideo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    /* Sin builder lo malo de esto es que cargara de una vez todos los videos y eso no es bueno para el perfomance */
+    // return PageView(
+    //   scrollDirection: Axis.vertical,
+    //   physics: const BouncingScrollPhysics(),
+    //   children: [
+    //     Container(color: Colors.red),
+    //     Container(color: Colors.blue),
+    //     Container(color: Colors.teal),
+    //     Container(color: Colors.yellow),
+    //     Container(color: Colors.pink),
+    //     Container(color: Colors.deepOrange),
+    //   ],
+    // );
+    /* El PageView con builder no recibe un children, recibe un itemBuilder */
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.teal),
-        Container(color: Colors.yellow),
-        Container(color: Colors.pink),
-        Container(color: Colors.deepOrange),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // Video Player + gradiente
+            VideoButtons(video: videoPost)
+          ],
+        );
+      },
     );
   }
 }
